1f1ada33d0e86017fde767f1aaa899f6e9914e79
Adam Wheeler <adamwhlr@gmail.com>
2025-09-09 12:03:32 -0400

    eliminate KorgFitParamsType

--- src/korg/fit.py
+++ src/korg/fit.py
@@ -5,23 +5,21 @@ from korg._python_interface import Array1dF64, KFloat, Linelist
 
 from ._julia_import import Korgjl
 
-type KorgFitParamsType = dict[str, KFloat] | dict[str, int] | dict[str, float]
-
 
 def fit_spectrum(
     obs_wls: Array1dF64,
     obs_flux: Array1dF64,
     obs_err: Array1dF64,
     linelist: Linelist,
-    initial_guesses: KorgFitParamsType,
-    fixed_params: KorgFitParamsType = {},
+    initial_guesses: dict[str, KFloat],
+    fixed_params: dict[str, KFloat] = {},
     *,
     windows: Iterable[tuple[KFloat, KFloat]] | None = None,
     R: KFloat | None | Callable[[KFloat], KFloat] = None,
     wl_buffer: KFloat = 1.0,
     precision: KFloat = 1e-4,
     time_limit: KFloat = 10_000,
-    adjust_continuum:bool = False,
+    adjust_continuum: bool = False,
     **synthesis_kwargs,
 ):
     Korgjl.Fit.fit_spectrum(
